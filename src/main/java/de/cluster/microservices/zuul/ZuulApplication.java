e863e4325fc73899923825e3f35cf6fb0820e2e5
ADESSO\guth <yannick.guth@adesso.de>
2017-12-05 12:30:54 +0100

    spring update

--- src/main/java/de/cluster/microservices/zuul/ZuulApplication.java
+++ src/main/java/de/cluster/microservices/zuul/ZuulApplication.java
@@ -1,6 +1,5 @@
 package de.cluster.microservices.zuul;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,20 +8,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
 @EnableZuulProxy
-@Slf4j
 @SpringBootApplication(scanBasePackages = {"de.cluster.microservices.zuul"} )
 @EnableDiscoveryClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx=null;
-		try{
-			ctx = SpringApplication.run(ZuulApplication.class, args);
-		}catch (Exception e) {
-			log.error(e.getStackTrace().toString());
-		}
-		finally {
-			((AbstractApplicationContext)ctx).registerShutdownHook();
-		}
+		SpringApplication.run(
+				ZuulApplication.class, args);
 	}
 }
